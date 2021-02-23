e7855b5a880cdd8a8d8e5379432a07d2941473d5
BuildTools <clayloneman@gmail.com>
2021-02-23 01:15:44 -0600

    new functions

--- functions/create-timer.js
+++ functions/create-timer.js
@@ -1,9 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const { nanoid } = require("nanoid");
-const co = require("co");
-
-let conn = null;
 
 const DB_URL = process.env.DB_URL;
 const DB_NAME = "timers";
@@ -29,11 +26,21 @@ const connect = () => {
   return mongoose.connect(`${DB_URL}/timers`);
 };
 
-
-//const Timer = mongoose.model("timer", timer);
+let timer = new mongoose.Schema({
+  _id: {
+    type: String,
+    default: nanoid(6),
+  },
+  title: String,
+  expireAt: {
+    type: Date,
+    default: Date.now,
+    index: { expires: "1d" },
+  },
+});
+const Timer = mongoose.model("timer", timer);
 
 exports.handler = async function (event, context, callback) {
-  context.callbackWaitsForEmptyEventLoop = false;
   // Check for data
   const data = JSON.parse(event.body);
   if (!data || !data.title || !data.expires) {
@@ -49,56 +56,16 @@ exports.handler = async function (event, context, callback) {
     title: data.title,
     expiresAt: data.expires,
   };
-  await run().
-    then(res => {
-      callback(null, res);
-    }).
-    catch(error => callback(error));
-};
-function run() {
-  let timer = new mongoose.Schema({
-    _id: {
-      type: String,
-      default: nanoid(6),
-    },
-    title: String,
-    expireAt: {
-      type: Date,
-      default: Date.now,
-      index: { expires: "1d" },
-    },
-  });
-  return co(function* () {
-    if (conn == null) {
-      conn = yield mongoose.createConnection(`${DB_URL}/timers`, {
-        bufferCommands: false,
-        bufferMaxEntries: 0,
+  
+  await connect()
+    .then(async connection => {
+      const makeTimer = await Timer.create(timer);
+      Timer.createIndexes;
+      return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(makeTimer),
       });
-      conn.model(
-        "timers",
-        timer
-      );
-    }
-
-    const M = conn.model("timers");
-
-    const doc = yield M.find();
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(doc),
-    };
-    console.log(response)
-    return response;
-  });
-}
-  // await connect()
-  //   .then(async connection => {
-  //     const makeTimer = await Timer.create(timer);
-  //     Timer.createIndexes;
-  //     return callback(null, {
-  //       statusCode: 200,
-  //       body: JSON.stringify(makeTimer),
-  //     });
       
-  //   })
-  // .catch(e => console.log(e));
+    })
+  .catch(e => console.log(e));
+};
---
+++ functions/get-timers.js
@@ -0,0 +1,41 @@
+const MongoClient = require("mongodb").MongoClient;
+
+const MONGODB_URI = process.env.DB_URL;
+const DB_NAME = "timers";
+
+let cachedDb = null;
+
+const connectToDatabase = async (uri) => {
+  // we can cache the access to our database to speed things up a bit
+  // (this is the only thing that is safe to cache here)
+  if (cachedDb) return cachedDb;
+
+  const client = await MongoClient.connect(uri, {
+    useUnifiedTopology: true,
+  });
+
+  cachedDb = client.db(DB_NAME);
+
+  return cachedDb;
+};
+
+const queryDatabase = async (db) => {
+  const timers = await db.collection("timers").find({}).toArray();
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(timers),
+  };
+};
+
+module.exports.handler = async (event, context) => {
+  // otherwise the connection will never complete, since
+  // we keep the DB connection alive
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const db = await connectToDatabase(MONGODB_URI);
+  return queryDatabase(db);
+};
