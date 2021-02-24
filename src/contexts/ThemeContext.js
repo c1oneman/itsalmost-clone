0c24976ae0df19a3b54fd9b00d801f6c22c8a39e
BuildTools <clayloneman@gmail.com>
2021-02-24 14:37:13 -0600

    default to light mode

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -4,8 +4,8 @@ export const ThemeContext = createContext();
 
 const ThemeContextProvider = (props) => {
   const [lightTheme, setLightTheme] = useState(() => {
-    const status = JSON.parse(localStorage.getItem("preferDark"));
-    return status ? true : false
+    const status = JSON.parse(localStorage.getItem("preferDark") ?? true);
+    return status
   });
   
   
